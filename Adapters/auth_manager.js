b5838b80187fcaf9db7f0ae0aa5f968e3ed1ba85
micxcode <micxcode@users.noreply.github.com>
2020-03-03 10:16:27 -0300

    escopo pronto

---
+++ Adapters/auth_manager.js
@@ -0,0 +1,16 @@
+class AuthManager {
+
+    constructor() {
+        this.db = new db_manager();
+    }
+
+    validateToken(token) {
+
+    }
+
+    validateAction(token) {
+
+    }
+}
+
+module.exports = AuthManager;
